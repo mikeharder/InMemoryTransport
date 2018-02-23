1eec4fce6791ab0d36d4c3890d9151589c291a31
Mike Harder <mharder@microsoft.com>
2018-02-23 15:52:43 -0800

    Sample app using InMemoryTransport

---
+++ InMemoryTransport/InMemoryTransportFactory.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Server.Kestrel.Transport.Abstractions.Internal;
+
+#if NETCOREAPP2_1
+using System.Buffers;
+using System.IO.Pipelines;
+#elif NETCOREAPP2_0
+using Microsoft.AspNetCore.Server.Kestrel.Internal.System.IO.Pipelines;
+#endif
+
+namespace InMemoryTransport
+{
+    public class InMemoryTransportFactory : ITransportFactory
+    {
+        public InMemoryConnection Connection { get; } = new InMemoryConnection();
+
+        public ITransport Create(IEndPointInformation endPointInformation, IConnectionHandler handler)
+        {
+            return new InMemoryTransport(handler, Connection);
+        }
+
+        private class InMemoryTransport : ITransport
+        {
+            private IConnectionHandler _handler;
+            private InMemoryConnection _connection;
+
+            public InMemoryTransport(IConnectionHandler handler, InMemoryConnection connection)
+            {
+                _handler = handler;
+                _connection = connection;
+            }
+
+            public Task BindAsync()
+            {
+#if NETCOREAPP2_1
+                _handler.OnConnection(_connection);
+#elif NETCOREAPP2_0
+                var connectionContext = _handler.OnConnection(_connection);
+                _connection.ConnectionId = connectionContext.ConnectionId;
+                _connection.Input = connectionContext.Input;
+                _connection.Output = connectionContext.Output;
+#endif
+                return Task.CompletedTask;
+            }
+
+            public Task StopAsync()
+            {
+                return Task.CompletedTask;
+            }
+
+            public Task UnbindAsync()
+            {
+                return Task.CompletedTask;
+            }
+        }
+#if NETCOREAPP2_1
+        public class InMemoryConnection : TransportConnection
+#elif NETCOREAPP2_0
+        public class InMemoryConnection : IConnectionContext, IConnectionInformation
+#endif
+        {
+
+#if NETCOREAPP2_0
+            public string ConnectionId { get; set; }
+
+            public IPipeWriter Input { get; set; }
+
+            public IPipeReader Output { get; set; }
+
+            public IPEndPoint RemoteEndPoint { get; set; }
+
+            public IPEndPoint LocalEndPoint { get; set; }
+
+            public PipeFactory PipeFactory { get; set; } = new PipeFactory();
+
+            public IScheduler InputWriterScheduler { get; set; }
+
+            public IScheduler OutputReaderScheduler { get; set; }
+#endif
+
+            public Task SendRequestAsync(string request)
+            {
+                return Input.WriteAsync(Encoding.UTF8.GetBytes(request));
+            }
+
+            public async Task<string> GetResponseAsync()
+            {
+                while (true)
+                {
+                    var result = await Output.ReadAsync();
+                    try
+                    {
+                        if (result.Buffer.Length >= 100)
+                        {
+                            return Encoding.UTF8.GetString(result.Buffer.ToArray());
+                        }
+                        else
+                        {
+#if NETCOREAPP2_1
+                            Output.AdvanceTo(result.Buffer.Start, result.Buffer.End);
+#elif NETCOREAPP2_0
+                            Output.Advance(result.Buffer.Start, result.Buffer.End);
+#endif
+                        }
+                    }
+                    finally
+                    {
+#if NETCOREAPP2_1
+                        Output.AdvanceTo(result.Buffer.End);
+#elif NETCOREAPP2_0
+                        Output.Advance(result.Buffer.End);
+#endif
+                    }
+                }
+            }
+
+#if NETCOREAPP2_0
+            public void OnConnectionClosed(Exception ex)
+            {
+            }
+
+            public void Abort(Exception ex)
+            {
+            }
+#endif
+        }
+    }
+}
---
+++ InMemoryTransport/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Server.Kestrel.Transport.Abstractions.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace InMemoryTransport
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+#if NETCOREAPP2_1
+            Console.WriteLine("netcoreapp2.1");
+#elif NETCOREAPP2_0
+            Console.WriteLine("netcoreapp2.0");
+#else
+#error Invalid TFM
+#endif
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .ConfigureServices(services => services.AddSingleton<ITransportFactory, InMemoryTransportFactory>())
+                .Configure(app => app.Run(context =>
+                {
+                    return context.Response.WriteAsync("Hello World!");
+                }))
+                .Build();
+
+            host.Start();
+
+            var connection = ((InMemoryTransportFactory)host.Services.GetRequiredService<ITransportFactory>()).Connection;
+
+            connection.SendRequestAsync("GET / HTTP/1.1\r\nHost: host:port\r\n\r\n").Wait();
+
+            Console.WriteLine(connection.GetResponseAsync().Result);
+
+            Console.ReadLine();
+        }
+    }
+}
